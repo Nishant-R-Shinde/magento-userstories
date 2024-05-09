9d72ef0b40490f73995e7f6b793acad586c0267d
Nishant Shinde <nishant.shinde@hbwsl.com>
2024-05-09 13:39:55 +0530

    userstory 3 done

--- app/code/Mage2tv/Userstory3/Observer/ProductViewObserver.php
+++ app/code/Mage2tv/Userstory3/Observer/ProductViewObserver.php
@@ -1,12 +1,17 @@
 <?php
 namespace Mage2tv\Userstory3\Observer;
+
 use Magento\Framework\Event\ObserverInterface;
-class ProductViewObserver implements ObserverInterface{
+
+class ProductViewObserver implements ObserverInterface
+{
     private $logger;
-    public function __construct(\Psr\Log\LoggerInterface $logger) {
+    public function __construct(\Psr\Log\LoggerInterface $logger)
+    {
         $this->logger = $logger;
     }
-    public function execute(\Magento\Framework\Event\Observer $observer) {
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
         $product = $observer->getEvent()->getProduct();
         if($product) {
             $productName = $product->getName();
--- app/code/Mage2tv/Userstory3/Plugin/LogPageHtml.php
+++ app/code/Mage2tv/Userstory3/Plugin/LogPageHtml.php
@@ -1,15 +1,20 @@
 <?php
 namespace Mage2tv\Userstory3\Plugin;
+
 use Magento\Catalog\Controller\Product\View;
 use Psr\Log\LoggerInterface;
-class LogPageHtml {
+
+class LogPageHtml
+{
     private $logger;
-    public function __construct(LoggerInterface $logger) {
+    public function __construct(LoggerInterface $logger)
+    {
         $this->logger = $logger;
     }
-    public function afterExecute(View $subject, $result) {
+    public function afterExecute(View $subject, $result)
+    {
         $html = $result->getLayout()->getOutput();
-        $this->logger->info("The page html is : ". $html);
+        $this->logger->info("The page html is : " . $html);
         return $result;
     }
 }
